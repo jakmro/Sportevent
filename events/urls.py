b48732c4be54935373ee95d6cbc908ae1ed834d1
Mateusz <zsuetam36@gmail.com>
2024-04-27 19:48:57 +0200

    fixing urls names

--- events/urls.py
+++ events/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('<int:pk>', EventView.as_view(), name='event'),
     path('add/', AddEventView.as_view(), name='add_event'),
     path('<int:pk>/update/', UpdateEventView.as_view(), name='update_event'),
-    path('<int:pk>/delete_event/', DeleteEventView.as_view(), name='delete_event'),
-    path('<int:pk>/register_for_event/', EventRegistrationView.as_view(), name='register_for_event')
+    path('<int:pk>/delete/', DeleteEventView.as_view(), name='delete_event'),
+    path('<int:pk>/register/', EventRegistrationView.as_view(), name='register_for_event')
 ]
