37877cda6971f8d7edff6adfcba78ff6aa1f5489
Mateusz <zsuetam36@gmail.com>
2024-04-27 13:30:14 +0200

    added events updating

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     EventsView,
-    AddEventView
+    AddEventView,
+    UpdateEventView
 )
 
 urlpatterns = [
     path('', EventsView.as_view(), name='events'),
     path('add/', AddEventView.as_view(), name='add_event'),
+    path('<int:pk>/update/', UpdateEventView.as_view(), name='update_event'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse, Http404
+from django.utils.translation import gettext
 
 from .forms import EventForm
-from .models import Event, Facility
+from .models import Event
 
 
 class EventsView(LoginRequiredMixin, ListView):
@@ -15,7 +17,29 @@ class AddEventView(LoginRequiredMixin, CreateView):
     model = Event
     form_class = EventForm
     template_name = 'events/add_event.html'
-    success_url = reverse_lazy('facilities')
+    success_url = reverse_lazy('events')
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class UpdateEventView(LoginRequiredMixin, UpdateView):
+    model = Event
+    fields = [
+        'name',
+        'description',
+        'sport_type',
+        'date',
+        'max_people_no',
+        'min_people_no',
+        'is_cyclic'
+    ]
+    template_name = 'events/update_event.html'
+    success_url = reverse_lazy('events')
+
+    def get_object(self, queryset=None):
+        obj = super(UpdateEventView, self).get_object(queryset)
+        if obj.user != self.request.user:
+            raise Http404(
+                gettext("It's not your event.")
+            )
+        return obj
