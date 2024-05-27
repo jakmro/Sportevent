6a5adb3f7e9de449b28de0018adce147b6de5051
Mateusz <zsuetam36@gmail.com>
2024-05-27 19:41:22 +0200

    added checking if event chat has started

--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponse
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.translation import gettext
-from events.models import EventRegistration, Event
+from django.utils import timezone
+from events.models import EventRegistration, Event, Meeting
+from datetime import timedelta
 
 def room(request, uidb64):
     room_name = urlsafe_base64_decode(uidb64).decode()
@@ -10,6 +12,12 @@ def room(request, uidb64):
     registration = EventRegistration.objects.filter(event=event, user=request.user)
     if not registration:
         raise Http404(
-            gettext("You are not registered on this event")
+            gettext("You are not registered for this event")
+        )
+    now = timezone.now()
+    meeting = Meeting.objects.filter(event=event).first()
+    if now + timedelta(days=1) < meeting.start_datetime:
+        raise Http404(
+            gettext("Chat for this event has not started yet")
         )
     return render(request, "chat/room.html", {"room_name": room_name})
