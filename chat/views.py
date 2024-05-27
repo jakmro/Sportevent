9f131723bcfbed537d527f8390c7382f49b7081c
Mateusz <zsuetam36@gmail.com>
2024-05-27 14:19:54 +0200

    added veryfying users on chat

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from django.http import Http404, HttpResponse
+from django.utils.translation import gettext
+from events.models import Event, EventRegistration, Meeting
+from .models import ChatMessage
 
 def room(request, room_name):
+    chat = ChatMessage.objects.get(id=room_name)
+    event = chat.meeting.event
+    registration = EventRegistration.objects.filter(event=event, user=request.user)
+    if not registration:
+        raise Http404(
+            gettext("You are not registered on this event")
+        )
     return render(request, "chat/room.html", {"room_name": room_name})
